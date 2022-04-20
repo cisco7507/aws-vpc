a6ec38424a052425d716f5ad79538c9a5b88f6f9
gsp <gsp@afilias.info>
2022-04-20 10:03:15 -0400

    ADD VPC gateway and VPC CGW to config  terraform fmt

--- root-module/main.tf
+++ root-module/main.tf
@@ -20,10 +20,10 @@ module "vpc" {
   cidr   = var.cidr
   azs    = local.azs
   # This is the Firewall Subnet
-  firewall_subnets = [for i in range(16, 18, 1) : cidrsubnet(var.cidr, 8, i)]
-  protected_subnets = [for i in range(2, 4, 1) : cidrsubnet(var.cidr, 4, i)]
-  private_subnets = [for i in range(4, 10, 1) : cidrsubnet(var.cidr, 4, i)]
-  enable_ipv6 = true
+  firewall_subnets       = [for i in range(16, 18, 1) : cidrsubnet(var.cidr, 8, i)]
+  protected_subnets      = [for i in range(2, 4, 1) : cidrsubnet(var.cidr, 4, i)]
+  private_subnets        = [for i in range(4, 10, 1) : cidrsubnet(var.cidr, 4, i)]
+  enable_ipv6            = true
   create_egress_only_igw = false
 
   /* Assigns IPv6 private subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). 
@@ -36,21 +36,21 @@ module "vpc" {
   enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true
-  
-  firewall_endpoint_ids  = module.firewall.firewall_endpoint_ids
-  
-  enable_vpn_gateway = true
-  propagate_private_route_tables_vgw = true
+
+  firewall_endpoint_ids = module.firewall.firewall_endpoint_ids
+
+  enable_vpn_gateway                  = true
+  propagate_private_route_tables_vgw  = true
   propagate_firewall_route_tables_vgw = true
   customer_gateways = {
     "ON1" = {
-      "bgp_asn" = "21775"
-      "ip_address" = "66.199.183.4"
-      "device_name" = "ON1-ASA" 
+      "bgp_asn"     = "21775"
+      "ip_address"  = "66.199.183.4"
+      "device_name" = "ON1-ASA"
     }
     "Office" = {
-      "bgp_asn" = "21775"
-      "ip_address" = "199.15.87.4"
+      "bgp_asn"     = "21775"
+      "ip_address"  = "199.15.87.4"
       "device_name" = "FTD"
     }
   }
@@ -65,7 +65,7 @@ module "vpc" {
   vpc_tags = {
     Name = "on1-dr-vpc"
   }
- 
+
 }
 
 module "firewall" {
